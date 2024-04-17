c155b97a36ba2158e1285f68d34eb94cbe590388
Wendy <139488528+WendyDarli@users.noreply.github.com>
2024-04-16 22:47:52 -0300

    Mongoose connection
     connection to the database

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//conect to database
+const mongoose = require('mongoose');
+
+mongoose.set("strictQuery", false);
+const mongoDB = 'mongodb+srv://inventorymanager:oEL3K5yLrPDMqwqD@cluster0.e1susyz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+main().catch((err) => console.log(err));
+async function main(){
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
