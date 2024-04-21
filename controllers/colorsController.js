63b8ab754f4e4aa9533731d6fe40de4df2215e6d
Wendy <139488528+WendyDarli@users.noreply.github.com>
2024-04-21 16:13:50 -0300

    all routes

--- controllers/colorsController.js
+++ controllers/colorsController.js
@@ -1,42 +1,42 @@
 const asyncHandler = require("express-async-handler");
 
 // Display list of all colors.
-exports.colorslist = asyncHandler(async (req, res, next) => {
+exports.colors_list = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: colorslist");
 });
 
 // Display detail page for a specific colors
-exports.colorsdetail = asyncHandler(async (req, res, next) => {
+exports.colors_detail = asyncHandler(async (req, res, next) => {
   res.send(`NOT IMPLEMENTED: colorsdetail: ${req.params.id}`);
 });
 
 // Display colorscreate form on GET.
-exports.colorscreate_get = asyncHandler(async (req, res, next) => {
+exports.colors_create_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: colorscreate GET");
 });
 
 // Handle colorscreate on POST.
-exports.colorscreate_post = asyncHandler(async (req, res, next) => {
+exports.colors_create_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: colorscreate POST");
 });
 
 // Display colorsdelete form on GET.
-exports.colorsdelete_get = asyncHandler(async (req, res, next) => {
+exports.colors_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: colorsdelete GET");
 });
 
 // Handle colorsdelete on POST.
-exports.colorsdelete_post = asyncHandler(async (req, res, next) => {
+exports.colors_delete_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: colorsdelete POST");
 });
 
 // Display colorsupdate form on GET.
-exports.colorsupdate_get = asyncHandler(async (req, res, next) => {
+exports.colors_update_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: colorsupdate GET");
 });
 
 // Handle colorsupdate on POST.
-exports.colorsupdate_post = asyncHandler(async (req, res, next) => {
+exports.colors_update_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: colorsupdate POST");
 });
    
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,66 @@ const item_controller = require('../controllers/itemsController');
 const categories_controller = require('../controllers/categoriesController');
 const colors_controller = require('../controllers/colorsController');
 
+//get home page
 router.get("/", item_controller.index);
+
+/// ITEM ROUTES ///
+
+// GET request for creating a item. NOTE This must come before routes that display item (uses id).
+router.get("/item/create", item_controller.item_create_get);
+
+// POST request for creating item.
+router.post("/item/create", item_controller.item_create_post);
+
+// GET request to delete item.
+router.get("/item/:id/delete", item_controller.item_delete_get);
+
+// POST request to delete item.
+router.post("/item/:id/delete", item_controller.item_delete_post);
+
+// GET request to update item.
+router.get("/item/:id/update", item_controller.item_update_get);
+
+// POST request to update item.
+router.post("/item/:id/update", item_controller.item_update_post);
+
+// GET request for one item.
+router.get("/item/:id", item_controller.item_detail);
+
+// GET request for list of all items.
+router.get("/items", item_controller.item_list);
+
+
+/// CATEGORIES ROUTES ///
+
+router.get("/category/create", categories_controller.category_create_get);
+
+router.post("/category/create", categories_controller.category_create_post);
+
+router.get("/category/:id/delete", categories_controller.category_delete_get);
+
+router.post("/category/:id/delete", categories_controller.category_delete_post);
+
+router.get("/category/:id/delete", categories_controller.category_delete_get);
+
+router.post("/category/:id/delete", categories_controller.category_delete_post);
+
+router.get("/category", categories_controller.category_list);
+
+/// COLORS ROUTES ///
+
+router.get("/color/create", colors_controller.colors_create_get);
+
+router.post("/color/create", colors_controller.colors_create_post);
+
+router.get("/color/:id/delete", colors_controller.colors_delete_get);
+
+router.post("/color/:id/delete", colors_controller.colors_delete_post);
+
+router.get("/color/:id/delete", colors_controller.colors_delete_get);
+
+router.post("/color/:id/delete", colors_controller.colors_delete_post);
+
+router.get("/color", colors_controller.colors_list);
 
 module.exports = router;
