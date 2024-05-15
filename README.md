9888becaaedd386069164df8d5b0a19b16f9d1cf
WendyDarli <139488528+WendyDarli@users.noreply.github.com>
2024-05-15 11:37:00 -0300

    removing some comments

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Inventory-management-app
 
-part of The Odin Project
+### Descrição
+Este projeto faz parte do curso NodeJS do The Odin Project. e implementa um sistema de gerenciamento de inventário utilizando Node.js e Express para o backend. Permititindo que os usuários realizem operações CRUD (Create, Read, Update, Delete) em itens do inventário.
+
+### Tecnologias Utilizadas:
+- **Node.js:** Ambiente de servidor backend para executar código JavaScript.
+- **Express:** Framework web para Node.js usado para roteamento e manipulação de requisições HTTP.
+- **MongoDB:** Banco de dados NoSQL usado para armazenar dados do inventário.
+- **Mongoose:** Ferramenta de modelagem de objetos do MongoDB usada para interagir com o banco de dados a partir do Node.js.
+- **Git:** Sistema de controle de versão para rastrear alterações no código do projeto.
+
+[Live Preview](URL)
--- routes/index.js
+++ routes/index.js
@@ -10,28 +10,20 @@ router.get("/", item_controller.index);
 
 /// ITEM ROUTES ///
 
-// GET request for creating a item. NOTE This must come before routes that display item (uses id).
 router.get("/item/create", item_controller.item_create_get);
 
-// POST request for creating item.
 router.post("/item/create", item_controller.item_create_post);
 
-// GET request to delete item.
 router.get("/item/:id/delete", item_controller.item_delete_get);
 
-// POST request to delete item.
 router.post("/item/:id/delete", item_controller.item_delete_post);
 
-// GET request to update item.
 router.get("/item/:id/update", item_controller.item_update_get);
 
-// POST request to update item.
 router.post("/item/:id/update", item_controller.item_update_post);
 
-// GET request for one item.
 router.get("/item/:id", item_controller.item_detail);
 
-// GET request for list of all items.
 router.get("/item", item_controller.item_list);
 
 
